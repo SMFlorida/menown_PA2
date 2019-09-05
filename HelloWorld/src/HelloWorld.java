1030af4eaf4a7cf92ac796c937c0ab8e77f54729
Shelby M <menownsl@knights.ucf.edu>
2019-09-05 02:29:16 -0400

    Add Code Comments

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,8 +1,16 @@
+/* Shelby Menown
+ * September 7, 2019
+ * COP3330 - Object Oriented Programming
+ * 
+ * This program will output the phrase "Hello, World!"
+ */
+
+
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Printing statement
 		System.out.println("Hello, World!");
 	}
 
